787daa9749f8d817c2e2113025b6e32186a2ac50
Idk0712 <132455294+Idk0712@users.noreply.github.com>
2023-07-09 23:53:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,92 +4,81 @@ const body = doc.body;
 const head = doc.head;
 let hue = 0;
 doc.addEventListener("keydown", e => {
-    if(e.key == "F12") e.preventDefault();
+    //if(e.key == "F12") e.preventDefault();
 });
 const div = doc.querySelector(".whoWonDiv");
 const robotsWins = doc.querySelector("#robotsWins");
 const playersWins = doc.querySelector("#playersWins");
 const games_count = doc.querySelector(".games_count");
 const tiesDiv = doc.querySelector(".ties");
-robotsWins.textContent = "🤖's wins: 0"
-playersWins.textContent = "player's wins: 0";
 const RPS = ["👊", "🖐️", "✌️"];
+let updateGame = JSON.parse(localStorage.getItem("update")) || {
+    playersWin: 0,
+    computersWins: 0,
+    gamesCount: 0,
+    ties: 0
+};
 let computerChoice;
-let playersWin = 0;
-let computersWins = 0;
-let gamesCount = 0;
-let ties = 0;
 doc.addEventListener("keydown", e => {
     if(e.key === "r" || e.key === "R") {
-        playersWin=0;
-        computersWins=0;
-        gamesCount=0;
-        ties=0;
-        tiesDiv.textContent = `Ties: ${ties}`;
-        games_count.textContent = `Games: ${gamesCount}`;
-        robotsWins.textContent = `🤖's wins: ${computersWins}`;
-        playersWins.textContent = `player's wins: ${playersWin}`;
+        localStorage.removeItem("update");
     }
 });
-function rockBtnClick(pChoice) {
-    gamesCount++;
-    games_count.textContent = `Games: ${gamesCount}`;
-    computerChoice = RPS[Math.floor(Math.random() * RPS.length)];
-    if(pChoice === RPS[0] && computerChoice === RPS[0]) {
-        ties++;
-        tiesDiv.textContent = `Ties: ${ties}`;
-        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
-    }
-    if(pChoice === RPS[0] && computerChoice === RPS[1]) {
-        computersWins++;
-        robotsWins.textContent = `🤖's wins: ${computersWins}`;
-        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
-    }
-    if(pChoice === RPS[0] && computerChoice === RPS[2]) {
-        playersWin++;
-        playersWins.textContent = `player's wins: ${playersWin}`;
-        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
-    }
-};
-function paperBtnClick(pChoice) {
-    gamesCount++;
-    games_count.textContent = `Games: ${gamesCount}`;
-    computerChoice = RPS[Math.floor(Math.random() * RPS.length)];
-    if(pChoice === RPS[1] && computerChoice === RPS[0]) {
-        playersWin++;
-        playersWins.textContent = `player's wins: ${playersWin}`;
-        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
-    }
-    if(pChoice === RPS[1] && computerChoice === RPS[1]) {
-        ties++;
-        tiesDiv.textContent = `Ties: ${ties}`;
-        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
-    }
-    if(pChoice === RPS[1] && computerChoice === RPS[2]) {
-        computersWins++;
-        robotsWins.textContent = `🤖's wins: ${computersWins}`;
-        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
-    }
-};
-function scissorsBtnClick(pChoice) {
-    gamesCount++;
-    games_count.textContent = `Games: ${gamesCount}`;
+tiesDiv.textContent = `Ties: ${updateGame.ties}`;
+games_count.textContent = `Games: ${updateGame.gamesCount}`;
+robotsWins.textContent = `🤖's wins: ${updateGame.computersWins}`;
+playersWins.textContent =  `player's wins: ${updateGame.playersWin}`;
+function playGame(pChoice){
+    updateGame.gamesCount++;
+    games_count.textContent = `Games: ${updateGame.gamesCount}`;
     computerChoice = RPS[Math.floor(Math.random() * RPS.length)];
     if(pChoice === RPS[2] && computerChoice === RPS[0]) {
-        computersWins++;
-        robotsWins.textContent = `🤖's wins: ${computersWins}`;
+        updateGame.computersWins++;
+        robotsWins.textContent = `🤖's wins: ${updateGame.computersWins}`;
         div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
     }
     if(pChoice === RPS[2] && computerChoice === RPS[1]) {
-        playersWin++;
-        playersWins.textContent = `player's wins: ${playersWin}`
+        updateGame.playersWin++;
+        playersWins.textContent = `player's wins: ${updateGame.playersWin}`
         div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
     }
     if(pChoice === RPS[2] && computerChoice === RPS[2]) {
-        ties++;
-        tiesDiv.textContent = `Ties: ${ties}`;
+        updateGame.ties++;
+        tiesDiv.textContent = `Ties: ${updateGame.ties}`;
         div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
     }
+    if(pChoice === RPS[1] && computerChoice === RPS[0]) {
+        updateGame.playersWin++;
+        playersWins.textContent = `player's wins: ${updateGame.playersWin}`;
+        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
+    }
+    if(pChoice === RPS[1] && computerChoice === RPS[1]) {
+        updateGame.ties++;
+        tiesDiv.textContent = `Ties: ${updateGame.ties}`;
+        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
+    }
+    if(pChoice === RPS[1] && computerChoice === RPS[2]) {
+        updateGame.computersWins++;
+        robotsWins.textContent = `🤖's wins: ${updateGame.computersWins}`;
+        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
+    }
+    if(pChoice === RPS[0] && computerChoice === RPS[0]) {
+        updateGame.ties++;
+        tiesDiv.textContent = `Ties: ${updateGame.ties}`;
+        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
+    }
+    if(pChoice === RPS[0] && computerChoice === RPS[1]) {
+        updateGame.computersWins++;
+        robotsWins.textContent = `🤖's wins: ${updateGame.computersWins}`;
+        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
+    }
+    if(pChoice === RPS[0] && computerChoice === RPS[2]) {
+        updateGame.playersWin++;
+        playersWins.textContent = `player's wins: ${updateGame.playersWin}`;
+        div.textContent = `You: ${pChoice}, 🤖: ${computerChoice}`;
+    }
+    localStorage.setItem("update", JSON.stringify(updateGame));
+    log(updateGame);
 };
 function changeHue() {
     hue+=Math.random() * 3;
